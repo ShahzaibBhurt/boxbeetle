57a4664644c6d694e1b1a8669fabc113f98144f7
ShahzaibBhurt <37025541+ShahzaibBhurt@users.noreply.github.com>
2022-04-04 01:55:08 +0500

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,51 @@
+$(document).ready(()=>{
+    $('footer').html(`
+    <div class="footer top_layer ">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-xl-5 col-lg-5 col-md-6 col-sm-12">
+                        <div class="address">
+                            <a href="index.html"> <img src="images/logo.png" alt="logo" /></a>
+                            <p>dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et sdolor sit amet, consectetur adipiscing elit, </p>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+                        <div class="address">
+                            <h3>Quick links</h3>
+                            <ul class="Links_footer">
+                                <li><img src="icon/3.png" alt="#" /> <a href="#"> Join Us</a> </li>
+                                <li><img src="icon/3.png" alt="#" /> <a href="#">Maintenance</a> </li>
+                                <li><img src="icon/3.png" alt="#" /> <a href="#">Language Packs</a> </li>
+                                <li><img src="icon/3.png" alt="#" /> <a href="#">LearnPress</a> </li>
+                                <li><img src="icon/3.png" alt="#" /> <a href="#">Release Status</a> </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                        <div class="address">
+                            <h3>Contact Us</h3>
+
+                            <ul class="loca">
+                                <li>
+                                    <a href="#"><img src="icon/loc.png" alt="#" /></a>London 145
+                                    <br>United Kingdom </li>
+                                <li>
+                                    <a href="mailto:info@boxbeetle.com"><img src="icon/email.png" alt="#" /> </a>info@boxbeetle.com</li>
+                                <!--<li>
+                                    <a href="#"><img src="icon/call.png" alt="#" /></a>+12586954775 </li>-->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     
+        <div class="copyright">
+            <div class="container">
+                <p>Copyright 2021 BoxBeetle International Couriers and Technology Solutions</p>
+           
+        </div>
+        </div>
+    `);
+})
